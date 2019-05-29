d384aaf668b13a343b0de4749a74e16e8fff2323
khbalaji21 <33088746+khbalaji21@users.noreply.github.com>
2019-05-29 15:51:06 +0530

    Assignment 7
    Assignment 7

---
+++ 07_JQuery_Assignment_1_399584/jQAss.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    $("#q1goTop").bind("click", function () {
+        window.scrollTo(0, 0);
+    });
+
+    $("#name1").bind("input", function () {
+        alert("First name is changed");
+    });
+    $("#name2").on("input", function () {
+        alert("Last name is changed");
+    });
+
+    $("#q3btn").click(function () {
+        $("q3btn").text('Changed!!!');
+    });
+
+    $("#q4myColors").append('<option value="Green">Green</option><option value="Orange">Orange</option>');
+
+    $('body').css('background-image', 'url("sample.jpg")');
+});
+
